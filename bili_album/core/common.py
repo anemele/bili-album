90c2e306aa9f929b4e6358b93b4f50547f89f013
Jason Swift <anemele@outlook.com>
2023-12-22 18:55:58 +0800

    use common FakeUA

--- bili_album/core/common.py
+++ bili_album/core/common.py
@@ -1,10 +1,12 @@
 import aiohttp
 from fake_useragent import FakeUserAgent
 
+_FakeUA = FakeUserAgent()
+
 
 def new_session():
     return aiohttp.ClientSession(
-        headers={'user-agent': FakeUserAgent().random},
+        headers={'user-agent': _FakeUA.random},
         timeout=aiohttp.ClientTimeout(total=600),  # 将超时时间设置为600秒
         connector=aiohttp.TCPConnector(limit=50),  # 将并发数量降低
     )
