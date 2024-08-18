48243124938a849e11028f11893df3bb8b0ec12f
Jason Swift <anemele@outlook.com>
2024-08-18 11:22:18 +0800

    add formatter for logger

--- bili_album/log.py
+++ bili_album/log.py
@@ -1,14 +1,21 @@
 import datetime
 import logging
+import sys
+
+logger = logging.getLogger(__package__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    fmt='%(asctime)s | %(levelname)-8s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
 
 now = datetime.datetime.now().strftime('%Y-%m-%d %H%M%S')
+fp = f'{__package__} {now}.log'
+fh = logging.FileHandler(filename=fp, encoding='utf-8')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
-logging.basicConfig(
-    format='%(asctime)s | %(levelname)-8s | %(message)s',
-    datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
-    encoding='utf-8',
-)
-logger = logging.getLogger(__package__)
-logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.FileHandler(f'{__package__} {now}.log'))
+sh = logging.StreamHandler(stream=sys.stdout)
+sh.setFormatter(formatter)
+logger.addHandler(sh)
