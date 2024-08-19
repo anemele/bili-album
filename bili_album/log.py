42180bc08acb04f53bf6ddfc6f79ce6f7e35fa40
Jason Swift <anemele@outlook.com>
2024-08-19 23:44:20 +0800

    update

--- bili_album/log.py
+++ bili_album/log.py
@@ -6,13 +6,13 @@ logger = logging.getLogger(__package__)
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter(
-    fmt='%(asctime)s | %(levelname)-8s | %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+    fmt="%(asctime)s | %(levelname)-8s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-now = datetime.datetime.now().strftime('%Y-%m-%d %H%M%S')
-fp = f'{__package__} {now}.log'
-fh = logging.FileHandler(filename=fp, encoding='utf-8')
+now = datetime.datetime.now().strftime("%Y-%m-%d")
+fp = f"{__package__} {now}.log"
+fh = logging.FileHandler(filename=fp, encoding="utf-8")
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
